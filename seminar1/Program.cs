27fd43d431088cb515b6db432cc1c89d5f59e6db
Lesya <neptun96@mail.ru>
2022-10-01 21:46:06 +0300

    answer to fird task added

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -52,3 +52,16 @@ if(n3 > max)
 Console.Write("the maximum of these numbers is ");
 Console.WriteLine(max);
 */
+
+// Задача 6. Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num % 2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
