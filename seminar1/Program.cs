ca2d20c16773f56b04cbbb84e4f1dd81c39a416b
Lesya <neptun96@mail.ru>
2022-10-01 21:41:41 +0300

    answer to second task added

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -26,3 +26,28 @@ else
 }
 Console.WriteLine($"max = {max} and min = {min}");
 */
+
+// Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Input a first number: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a fird number: ");
+int n3 = Convert.ToInt32(Console.ReadLine());
+
+int max = n1;
+if(n1 > max)
+{
+    max = n1;
+}
+if(n2 > max)
+{
+    max = n2;
+}
+if(n3 > max)
+{
+    max = n3;
+}
+Console.Write("the maximum of these numbers is ");
+Console.WriteLine(max);
