3ada9ef4a2ca20e04d80be38fa3fa98fc10892b9
Lesya <neptun96@mail.ru>
2022-10-08 22:56:06 +0300

    answer to fird task added

--- seminar2/Program.cs
+++ seminar2/Program.cs
@@ -43,3 +43,46 @@ int number = Convert.ToInt32(Console.ReadLine());
 int firdDigitNumber = FirdDigit(number);
 Console.WriteLine($"fird digit of {number} is {firdDigitNumber}");
 */
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+
+//Вариант 1 - bool
+/*
+bool CheckForWeekend(int week)
+{
+    if(week == 6 || week == 7)
+        return true;
+    else
+        return false;
+}
+
+Console.Write("Enter the day of the week in a number: ");
+int day = Convert.ToInt32(Console.ReadLine());
+//bool result = CheckForWeekend(day);
+//Console.WriteLine($"Entered day {day} of the weekend? {result}");
+Console.WriteLine($"Entered day {day} of the weekend?");
+Console.Write(CheckForWeekend(day));
+*/
+
+//Вариант 2 - string
+
+string CheckForWeekend(int week)
+{
+    if(week < 1 || week >= 8)
+        return "error - it is not day of week";
+    else
+    {
+        if(week == 6 || week == 7)
+            return "it is weekend";
+        else
+            return "it is work day";
+    }
+}
+
+Console.Write("Enter the day of the week in a number: ");
+int day = Convert.ToInt32(Console.ReadLine());
+string result = CheckForWeekend(day);
+Console.WriteLine($"Entered day {day} of the weekend?");
+Console.WriteLine(result);
+
