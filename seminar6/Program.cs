9cc76d7299c32449d877b487ee5e8e992b1f0d5d
Lesya <neptun96@mail.ru>
2022-10-21 19:51:56 +0300

    answer to sixth (one) task added

---
+++ seminar6/Program.cs
@@ -0,0 +1,19 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int PositiveNumbers()
+{
+    int count = 0;
+    Console.Write("Enter the number of elements: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    for(int i = 0; i < size; i++)
+    {
+        Console.WriteLine("Input the number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if(number > 0)
+            count++;
+    } 
+    return count;
+}
+
+int result = PositiveNumbers();
+Console.Write($"You entered {result} positive numbers");
