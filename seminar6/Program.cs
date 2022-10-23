8503c6c61de21c2acc08e2ede40ab267ce59315f
Lesya <neptun96@mail.ru>
2022-10-23 14:50:14 +0300

    answer to sixth task added

--- seminar6/Program.cs
+++ seminar6/Program.cs
@@ -1,5 +1,5 @@
-﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
-
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+/*
 int PositiveNumbers()
 {
     int count = 0;
@@ -17,3 +17,34 @@ int PositiveNumbers()
 
 int result = PositiveNumbers();
 Console.Write($"You entered {result} positive numbers");
+*/
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+/*
+void IntersectionPoint()
+{
+    double x = 0;
+    double y = 0;
+
+    Console.Write("Enter a point b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter a point k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter a point b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Enter a point k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * x + b1;
+
+    x = Math.Round(x, 2);
+    y = Math.Round(y, 2);
+
+    Console.WriteLine($"Intersection point x is {x}");
+    Console.WriteLine($"Intersection point y is {y}");
+}
+
+IntersectionPoint();
+*/
