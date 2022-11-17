a9753cdb6f241ebc13346812e3209f7375ac9efd
Lesya161 <114298712+Lesya161@users.noreply.github.com>
2022-11-17 13:21:44 +0300

    Update Program.cs

--- seminar9/Program.cs
+++ seminar9/Program.cs
@@ -37,7 +37,7 @@ int Akkerman (int n, int m)
 if (n == 0) return m + 1;
 else
     {
-        if ((n != 0) && (m == 0) return Akkerman (n - 1, 1);
+        if ((n != 0) && (m == 0)) return Akkerman (n - 1, 1);
         else return Akkerman (n - 1, Akkerman (n, m - 1);
      }
 }
