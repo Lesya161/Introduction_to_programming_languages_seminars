31a683006a12144f507ffb2503ab22f8f37c76d7
Lesya161 <114298712+Lesya161@users.noreply.github.com>
2022-11-17 13:18:34 +0300

    Update Program.cs

--- seminar9/Program.cs
+++ seminar9/Program.cs
@@ -13,10 +13,10 @@ ShowNumbers(number);
 
 //Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
 /*
-int SumNumbersSum(int m, int n)
+int SumNumbers(int m, int n)
 {
-    if (m > n) return n += SumNumbersSum(n + 1, m);
-    if (n > m) return n += SumNumbersSum(n - 1, m);
+    if (m > n) return n = n + SumNumbers(n + 1, m);
+    if (m < n) return n = n + SumNumbers(n - 1, m);
     else
         return n;
 }
@@ -31,3 +31,20 @@ Console.WriteLine(result);
 */
 
 //Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int Akkerman (int n, int m)
+{
+if (n == 0) return m + 1;
+else
+    {
+        if ((n != 0) && (m == 0) return Akkerman (n - 1, 1);
+        else return Akkerman (n - 1, Akkerman (n, m - 1);
+     }
+}
+
+Console.WriteLine("Input a number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int result = Akkerman(number1, number2);
+Console.WriteLine(result);
